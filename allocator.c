d17e6a37031a414504dc336bbe82e8a304b64983
OtmaneDaoudi <daoudiotman22@gmail.com>
2023-01-02 17:29:35 +0100

    fixed block merging

--- allocator.c
+++ allocator.c
@@ -1,10 +1,10 @@
 #include "allocator.h"
 
-list blocks_list = {.base = NULL, .end = NULL};  
+list blocks_list = {NULL, NULL}; 
 
 block_meta_data* find_block(size_t size)
 {
-    block_meta_data* current = blocks_list.base;
+    block_meta_data* current = blocks_list.base; 
     while(current != NULL && (!current->isFree || current->size < size)) 
     {
         current = current->next;
@@ -29,16 +29,13 @@ int extendHeap(size_t size)
         blocks_list.end->next = newBlock; 
         blocks_list.end = newBlock; 
     }
-    // printf("heap extended by creating a new block at %p of size = %lu.\n", newBlock, newBlock->size);
     return 1; 
 }
 
 
 void splitBlock(block_meta_data* block, size_t requestedSize)
 {
-    printf("split block\n"); 
-    //assuming that the block fits fo the requested size , and the passed
-    //requested size is multiple of four
+    //assuming that the block fits the requested size , and the passed requested size is aligned
     char* bytePointer = (char*)block; 
     block_meta_data* nextBlock = block->next; 
     block->next = (block_meta_data*)(bytePointer + BLOCK_SIZE + requestedSize); 
@@ -46,13 +43,12 @@ void splitBlock(block_meta_data* block, size_t requestedSize)
     block->next->next = nextBlock; 
     block->next->isFree = 1; 
     block->size = requestedSize; 
-    // printf("block at %p was split into two blocks : \n", block); 
-    // printf("\tblock1 at %p with size = %lu\n", block, block->size); 
-    // printf("\tblock2 at %p with size = %lu\n", block->next, block->next->size); 
+
+    //update last block in the list
+    if(nextBlock == NULL) blocks_list.end = block->next; 
 }
 
-
-void* malloc(size_t size)
+void* malloc_v2(size_t size)
 {
     if(size == 0) return NULL; 
     size_t alignedSize = align4(size); 
@@ -62,12 +58,13 @@ void* malloc(size_t size)
     {
         //chunk found
         //check for splits
-        size_t availableSpace = chunk->size - alignedSize - BLOCK_SIZE; 
+        int availableSpace = (int)chunk->size - (int)alignedSize - BLOCK_SIZE; 
         if(availableSpace >= 4)
         {
             //chunk is splitable AND the remaining chunk is aligned
             splitBlock(chunk, alignedSize); 
         }
+        chunk->isFree = 0;
         return ((void*)chunk) + BLOCK_SIZE;   
     }
     else
@@ -81,12 +78,75 @@ void* malloc(size_t size)
     }
 }
 
-
-/*
--causes a search of the free list, to find the proper place to insert the block being freed.
-- If the block being freed is adjacent to a free block on either side, it is coalesced with it into a single bigger block, so storage does not become too fragmented.
-*/
-void free(void * ptr)
+void showBlock()
 {
+    block_meta_data* current = blocks_list.base;
+    int i = 0; 
+    while(current != NULL)
+    {
+        printf("block %d at %p :\n", i++, current);   
+        printf("\tsize = %lu | isFree = %d | next = %p\n", current->size, current->isFree, current->next); 
+        current = current->next; 
+    }
+    printf("\n"); 
+}
 
+void* calloc_v2(size_t numberOfElements, size_t sizeOfEach)
+{
+    void* mem = malloc_v2(numberOfElements * sizeOfEach); 
+    //initialize the whole block to 0s
+    size_t bytes = align4(numberOfElements * sizeOfEach); 
+    for(int i=0; i<bytes; i++) *(((char*)mem) + i) = 0; 
+    return mem; 
+}
+
+void mergeBlocks(block_meta_data* target, block_meta_data* block)
+{
+    target->next = block->next; 
+    target->size = target->size + BLOCK_SIZE + block->size;
+}
+
+void nofrag(block_meta_data* block, block_meta_data* prev)
+{
+    //block is assured to be free at this point
+    block_meta_data* next = block->next; 
+    if(prev != NULL && prev->isFree)
+    {
+        //merge with prev
+        mergeBlocks(prev, block); 
+        block = prev; 
+    }
+    if(next != NULL && next->isFree) mergeBlocks(block, next); //merge with next
+}
+
+void free_v2(void* mem)
+{
+    if(mem == NULL) return; 
+    //assuring that mem is previously allocated by malloc_v2
+    block_meta_data* block = (block_meta_data*)(((char*)mem) - BLOCK_SIZE);
+    int isAllocated = 0; 
+    block_meta_data* current = blocks_list.base, *prev = NULL; 
+    while(current != NULL)
+    {
+        if(block == current)
+        {
+            isAllocated = 1; 
+            break; 
+        }
+        prev = current; 
+        current = current->next; 
+    }
+    if(isAllocated)
+    {
+        if(!block->isFree) 
+        {
+            block->isFree = 1;
+            nofrag(block, prev); 
+        }
+        else printf("Info : the block is already free_v2 and available for storage.\n");  
+    }
+    else
+    {
+        fprintf(stderr, "error free_v2ing a block that is not previously allocated by malloc_v2.\n"); 
+    }
 }
